850f201964ee3e553ab1097aa861c32e36c871de
greenmmq <mark@greenpnw.org>
2023-06-01 10:01:52 -0700

    MVP alex2mongo Pipeline
    TODO: debug the errors
    working connection from openalex queries to MongoDB Atlas
    contact greenmmq for creds.json and
    to get ip address whitelisted for mongodb access

--- src/alex2mongo.py
+++ src/alex2mongo.py
@@ -3,6 +3,7 @@
 # Mark Green - 5/23/23
 
 import json
+from bson import BSON
 from pprint import pprint
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -14,16 +15,18 @@ def mongoConnect():
         creds = json.load(f)
 
     # connect to OpenAlex API
-    pyalex.config.email = creds[0]  # your email here
+    pyalex.config.email = creds["email"]  # your email here
     # set mongo uri variables
-    uid = creds[1]
-    pwd = creds[2]
-    cluster = creds[3]
+    uid = creds["uid"]
+    pwd = creds["pwd"]
+    cluster = creds["cluster"]
 
     uri = f"mongodb+srv://{uid}:{pwd}@{cluster}.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
+    database = client["OpenAlexJournalArticles"]
+    collection = database["Works"]
 
     # Send a ping to confirm a successful connection
     try:
@@ -32,16 +35,25 @@ def mongoConnect():
     except Exception as e:
         print(e)
 
+    return collection
+
 
 def main():
-    result = pyalex.Works() \
-        .filter(institutions={"is_global_south":True}) \
-        .filter(primary_location={"source":{"id":"S13479253"}}) \
-        .group_by("institutions.country_code") \
-        .get()
 
-    pprint(result)
- 
+    collection = mongoConnect()
+
+    journal_articles = pyalex.Works().filter(primary_location={"source":{"id":"S13479253"}}).get(100)
+    print(f"Results length: {len(journal_articles)}\nInserting documents...")
+    i=0
+    for docs in journal_articles:
+       for d in docs:  # iterate over key-value pairs
+            print(type(BSON.encode(dict(d))))
+            print(type(docs))
+            print(type(journal_articles))
+            collection.insert_one(d) # your collection object here
+            i+=1
+    print(f"Successfully inserted {i} documents!")
+    
 
 if __name__ == "__main__":
     main()
