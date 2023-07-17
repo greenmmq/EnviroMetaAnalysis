40bf09256b13762563402bf84026f5f80f4574a2
greenmmq <mark@greenpnw.org>
2023-07-17 08:44:19 -0700

    add test.py
    add test.py to test mongodb localhost connection

---
+++ src/test.py
@@ -0,0 +1,9 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb://localhost:27017/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+database = client["OpenAlexJournals"]
+collection = database["works"]
+
+print(f"Total number of Articles: {collection.count_documents({})}")
